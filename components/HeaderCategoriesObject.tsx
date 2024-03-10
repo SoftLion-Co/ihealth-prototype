db2755f425dde9ad8ac0f7619fbfc8d1deaf739c
Paul <115890528+PaulGraur@users.noreply.github.com>
2024-03-10 12:22:18 +0200

    feature: add object component and create list for categories

---
+++ components/HeaderCategoriesObject.tsx
@@ -0,0 +1,118 @@
+import React from "react";
+
+const HeaderCategoriesObject = [
+  {
+    text: "Health and Care",
+    href: "/",
+    subcategories: [
+      {
+        title: "Чоловіче здоров'я",
+        items: [
+          { subText: "Мультивітаміни для чоловіків", href: "/" },
+          { subText: "Тестостерон", href: "/" },
+          { subText: "ДГЕА", href: "/" },
+          { subText: "Засоби для чоловіків", href: "/" },
+          { subText: "Чоловіча фертильність", href: "/" },
+          { subText: "Якірці", href: "/" },
+        ],
+      },
+      {
+        title: "Жіноче здоров'я",
+        items: [
+          { subText: "Мультивітаміни для жінок", href: "/" },
+          { subText: "Фолієва кислота", href: "/" },
+          { subText: "Жіночі гормональні препарати", href: "/" },
+          { subText: "Мультивітаміни для вагітних", href: "/" },
+          { subText: "ДГЕА", href: "/" },
+          { subText: "Жіноча фертильність", href: "/" },
+          { subText: "Продукти з прогестероном", href: "/" },
+          { subText: "Циміцифуга гілляста", href: "/" },
+        ],
+      },
+      {
+        title: "Дитяче здоров'я",
+        items: [
+          { subText: "Мультивітаміни для дітей", href: "/" },
+          { subText: "Пробіотики", href: "/" },
+          { subText: "Вітамін D", href: "/" },
+          { subText: "Вітамін C", href: "/" },
+          { subText: "Кальцій", href: "/" },
+          { subText: "ДГК та Омега", href: "/" },
+        ],
+      },
+    ],
+  },
+
+  {
+    text: "Supplements and Tools",
+    href: "/",
+    subcategories: [
+      {
+        title: "Добавки",
+        items: [
+          { subText: "Амінокислоти", href: "/" },
+          { subText: "Антиоксиданти", href: "/" },
+          { subText: "Прополіс", href: "/" },
+          { subText: "Біотин", href: "/" },
+          { subText: "Бета-глюкан", href: "/" },
+          { subText: "Кістки та суглоби", href: "/" },
+          { subText: "Кальцій", href: "/" },
+          { subText: "Колаген", href: "/" },
+          { subText: "Клітковина", href: "/" },
+          { subText: "Риб'ячий жир", href: "/" },
+          { subText: "Гіалуронова кислота", href: "/" },
+          { subText: "Вітаміни (А, B, C, D, E)", href: "/" },
+          { subText: "Цинк", href: "/" },
+          { subText: "Магній", href: "/" },
+        ],
+      },
+      {
+        title: "Трави",
+        items: [
+          { subText: "Ашваганда", href: "/" },
+          { subText: "Гінко білоба", href: "/" },
+          { subText: "Женьшень", href: "/" },
+          { subText: "Мака", href: "/" },
+          { subText: "Сереноя", href: "/" },
+          { subText: "Звіробій", href: "/" },
+        ],
+      },
+    ],
+  },
+
+  {
+    text: "Sports and Beauty",
+    href: "/",
+    subcategories: [
+      {
+        title: "Спортивне харчування",
+        items: [
+          { subText: "BCAA", href: "/" },
+          { subText: "Креатин", href: "/" },
+          { subText: "Спортивні батончики", href: "/" },
+          { subText: "Спортивні мультивітаміни", href: "/" },
+          { subText: "Сироватковий протеїн", href: "/" },
+          { subText: "Олія з МСТ", href: "/" },
+          { subText: "L-аргінін", href: "/" },
+          { subText: "L-карнітин", href: "/" },
+          { subText: "L-глютамін", href: "/" },
+        ],
+      },
+      {
+        title: "Товари для краси",
+        items: [
+          { subText: "Очищувальні засоби", href: "/" },
+          { subText: "Маски та пілінги для обличчя", href: "/" },
+          { subText: "Корейська косметика", href: "/" },
+          { subText: "Догляд за губами", href: "/" },
+          { subText: "Макіяж", href: "/" },
+          { subText: "Пензлі та інше для макіяжу", href: "/" },
+          { subText: "Зволожувальні засоби для обличчя", href: "/" },
+          { subText: "Засоби догляду та сироватки", href: "/" },
+        ],
+      },
+    ],
+  },
+];
+
+export default HeaderCategoriesObject;
