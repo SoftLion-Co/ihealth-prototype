bb3fe36cd5ff17c5314b20de5650bc0e6844e5eb
Yurii Matvii <tahkict.yup4ik@gmail.com>
2024-03-12 16:53:38 +0200

    feature: additional data props

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { useTranslations } from "next-intl";
 import ProductCardComponent from "@/components/ProductCardComponent";
+import test from "@/images/test.webp";
 
 const page = () => {
   const t = useTranslations("home_page");
+  const images = [test, test, test, test];
+  const slug = "product-slug"
+  const name = "WHEY Protein 100% Gold Standard"
+  const price = 20.40
 
   return (
     <main>
       <h1>{t("title")}</h1>
-      <ProductCardComponent rating={5} discount={20} />
+      <ProductCardComponent rating={5} discount={50} images={images} slug={slug} name={name} price={price} />
     </main>
   );
 };
--- components/ProductCardComponent.tsx
+++ components/ProductCardComponent.tsx
@@ -1,7 +1,6 @@
 "use client";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import React, { useState } from "react";
-import test from "@/images/test.webp";
 import { Rating } from "@mantine/core";
 import { Badge } from "@mantine/core";
 import starFilled from "@/images/navigation/StarFilled.svg";
@@ -10,22 +9,45 @@ import wishFilled from "@/images/navigation/WishFilled.svg";
 import wishOutline from "@/images/navigation/WishOutline.svg";
 import { Carousel } from "@mantine/carousel";
 import arrow from "@/images/vector/ChevronArrow.svg";
+import Link from "next/link";
+import { useLocale } from "next-intl";
+
 type Props = {
   rating?: 0 | 1 | 2 | 3 | 4 | 5;
   discount?: number;
   wishlist?: boolean;
+  images: StaticImageData[];
+  slug: string;
+  name: string;
+  price: number;
 };
 
 const ProductCardComponent = ({
   rating,
   discount,
   wishlist = false,
+  images,
+  slug,
+  name,
+  price,
 }: Props) => {
   const [isWishlistActive, setWishlistActive] = useState(wishlist);
+  const locale = useLocale();
 
   const toggleWishlist = () => {
     setWishlistActive(!isWishlistActive);
   };
+
+  const Control = ({ position }: { position: "next" | "prev" }) => (
+    <Image
+      src={arrow}
+      alt="Arrow"
+      width={24}
+      height={24}
+      className={position === "next" ? "-rotate-90" : "rotate-90"}
+    />
+  );
+
   return (
     <div className="bg-white w-[390px] group">
       <div className="relative">
@@ -35,52 +57,22 @@ const ProductCardComponent = ({
             control:
               "border-none shadow-none opacity-0 group-hover:opacity-100 transition",
           }}
-          nextControlIcon={
-            <Image
-              src={arrow}
-              alt="Arrow"
-              width={24}
-              height={24}
-              className="-rotate-90"
-            />
-          }
-          previousControlIcon={
-            <Image
-              src={arrow}
-              alt="Arrow"
-              width={24}
-              height={24}
-              className="rotate-90"
-            />
-          }
+          nextControlIcon={<Control position="next" />}
+          previousControlIcon={<Control position="prev" />}
         >
-          <Carousel.Slide>
-            <Image
-              src={test}
-              width={390}
-              height={440}
-              alt="Product Image"
-              className="h-[440px] object-cover object-center rounded"
-            />
-          </Carousel.Slide>
-          <Carousel.Slide>
-            <Image
-              src={test}
-              width={390}
-              height={440}
-              alt="Product Image"
-              className="h-[440px] object-cover object-center rounded"
-            />
-          </Carousel.Slide>
-          <Carousel.Slide>
-            <Image
-              src={test}
-              width={390}
-              height={440}
-              alt="Product Image"
-              className="h-[440px] object-cover object-center rounded"
-            />
-          </Carousel.Slide>
+          {images.map((image, index) => (
+            <Carousel.Slide key={index}>
+              <Link href={`/${locale}/${slug}`}>
+                <Image
+                  src={image}
+                  width={390}
+                  height={440}
+                  alt="Product Image"
+                  className="h-[440px] object-cover object-center rounded"
+                />
+              </Link>
+            </Carousel.Slide>
+          ))}
         </Carousel>
 
         <div className="absolute top-0 flex items-center justify-between w-full p-4">
@@ -132,16 +124,19 @@ const ProductCardComponent = ({
           />
         </button>
       </div>
-
-      <div className="p-4 flex flex-col">
-        <h2 className="text-lg text-[#424551]">
-          Shirt with insertion lace trims
-        </h2>
-        <div className="flex gap-3">
-          <span className="text-2xl	text-danger font-bold	">$20.40</span>
-          <span className="line-through text-[#787A80] text-lg">$40.80</span>
+      <Link href={`/${locale}/${slug}`}>
+        <div className="p-4 flex flex-col">
+          <h2 className="text-lg text-[#424551]">{name}</h2>
+          <div className="flex gap-3">
+            <span className="text-2xl	text-danger font-bold	">${price.toFixed(2)}</span>
+            {discount && (
+              <span className="line-through text-[#787A80] text-lg">
+                ${(price / (1 - discount / 100)).toFixed(2)}
+              </span>
+            )}
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
