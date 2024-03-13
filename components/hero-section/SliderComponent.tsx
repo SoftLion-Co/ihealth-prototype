3bd52f9a841b349f35e78752268610692f903a06
Yan Zhabko <113374407+yanzhabko@users.noreply.github.com>
2024-03-13 09:23:34 +0100

    feature: add autoplay for slider

--- components/hero-section/SliderComponent.tsx
+++ components/hero-section/SliderComponent.tsx
@@ -1,9 +1,11 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { Carousel } from "@mantine/carousel";
-import ButtonComponent from "./Slider/ButtonComponent";
+import Autoplay from "embla-carousel-autoplay";
+import ButtonComponent from "@/components/ButtonComponent";
 import ControlComponent from "./ControlComponent";
 import Image from "next/image";
+import Link from "next/link";
 
 interface SliderProps {
   slides: {
@@ -23,6 +25,7 @@ interface SliderProps {
 }
 
 const SliderComponent: React.FC<SliderProps> = ({ slides }) => {
+  const autoplay = useRef(Autoplay({ delay: 5000 }));
   useEffect(() => {
     const timeoutId = setTimeout(() => {
       const buttons = document.querySelectorAll<HTMLButtonElement>(
@@ -44,69 +47,79 @@ const SliderComponent: React.FC<SliderProps> = ({ slides }) => {
   }, []);
 
   return (
-    <Carousel
-      slideSize={"100%"}
-      align="start"
-      withIndicators
-      loop
-      previousControlIcon={
-        <ControlComponent section="slider" position="prev" alt="Previous" />
-      }
-      nextControlIcon={
-        <ControlComponent section="slider" position="next" alt="Next" />
-      }
-      classNames={{
-        root: "relative w-full h-[600px] md:h-[800px] bg-pink-100",
-        viewport: "w-full h-full",
-        container: "h-full w-full",
-        controls: "hidden md:flex",
-        control: "bg-white h-12 w-12",
-        indicators:
-          "bottom-[20%] flex flex-row gap-1 md:justify-start container",
-        indicator: `${
-          slides.length > 4
-            ? "w-[calc((100%-(4*15%))/4)]"
-            : "w-[calc(90%/4)] sm:w-[17%] lg:w-[20%]"
-        } h-[.210rem] bg-gray-700 opacity-40 before:text-[22px] before:text-[28px] before:font-bold before:text-gray-700 before:block before:opacity-80 before:absolute before:bottom-[15px] lg:before:bottom-[30px]`,
-        slide: "w-full",
-      }}
-    >
-      {slides.map((slide, index) => (
-        <Carousel.Slide key={index} className="w-full flex flex-start relative">
-          <Image
-            src={slide.img}
-            width={100}
-            height={800}
-            alt="Slide"
-            className="h-full w-full object-cover"
-          />
-          <div className="absolute top-[19%] lg:top-[25%] w-full">
-            <div className="container w-full flex flex-col gap-9">
-              <div className="flex flex-col gap-3 md:items-start text-center">
-                <h2 className="w-auto uppercase text-base lg:text-lg font-bold text-zinc-800">
-                  {slide.subTitle}
-                </h2>
-                <h1 className="text-[52px] lg:text-[72px] font-black text-zinc-800">
-                  {slide.title}
-                </h1>
-              </div>
-              <div className="flex gap-6 items-center justify-center md:justify-start">
-                <ButtonComponent
-                  type="toSale"
-                  href={slide.btnSale.href}
-                  title={slide.btnSale.title}
-                />
-                <ButtonComponent
-                  type="toCategory"
-                  href={slide.btnShop.href}
-                  title={slide.btnShop.title}
-                />
+    <>
+      <Carousel
+        slideSize={"100%"}
+        align="start"
+        withIndicators
+        loop
+        plugins={[autoplay.current]}
+        onMouseEnter={autoplay.current.stop}
+        onMouseLeave={autoplay.current.reset}
+        previousControlIcon={
+          <ControlComponent section="slider" position="prev" alt="Previous" />
+        }
+        nextControlIcon={
+          <ControlComponent section="slider" position="next" alt="Next" />
+        }
+        classNames={{
+          root: "relative w-full h-[600px] md:h-[800px] bg-pink-100",
+          viewport: "w-full h-full",
+          container: "h-full w-full",
+          controls: "hidden md:flex",
+          control: "bg-white h-12 w-12",
+          indicators:
+            "bottom-[20%] flex flex-row gap-1 md:justify-start container",
+          indicator: `${
+            slides.length > 4
+              ? "w-[calc((100%-(4*15%))/4)]"
+              : "w-[calc(90%/4)] sm:w-[17%] lg:w-[20%]"
+          } h-[.210rem] bg-gray-700 opacity-40 before:text-[22px] before:text-[28px] before:font-bold before:text-gray-700 before:block before:opacity-80 before:absolute before:bottom-[15px] lg:before:bottom-[30px]`,
+          slide: "w-full",
+        }}
+      >
+        {slides.map((slide, index) => (
+          <Carousel.Slide
+            key={index}
+            className="w-full flex flex-start relative"
+          >
+            <Image
+              src={slide.img}
+              width={100}
+              height={800}
+              alt="Slide"
+              className="h-full w-full object-cover"
+            />
+            <div className="absolute top-[19%] lg:top-[25%] w-full">
+              <div className="container w-full flex flex-col gap-9">
+                <div className="flex flex-col gap-3 md:items-start text-center">
+                  <h2 className="w-auto uppercase text-base lg:text-lg font-bold text-zinc-800">
+                    {slide.subTitle}
+                  </h2>
+                  <h1 className="text-[52px] lg:text-[72px] font-black text-zinc-800">
+                    {slide.title}
+                  </h1>
+                </div>
+                <div className="flex gap-6 items-center justify-center md:justify-start">
+                  <Link href={slide.btnSale.href}>
+                    <ButtonComponent
+                      text={slide.btnSale.title}
+                      typeButton="MainButton"
+                    />
+                  </Link>
+                  <Link href={slide.btnShop.href}>
+                    <ButtonComponent
+                      text={slide.btnShop.title}
+                      typeButton="MainBorderButton"
+                    />
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-        </Carousel.Slide>
-      ))}
-    </Carousel>
+          </Carousel.Slide>
+        ))}
+      </Carousel>
+    </>
   );
 };
 
