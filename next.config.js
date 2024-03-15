79965baf013215219ba3301773981aea296b1389
Yurii Matvii <tahkict.yup4ik@gmail.com>
2024-03-15 13:16:42 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,20 +5,6 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "content.rozetka.com.ua",
-      },
-      {
-        protocol: "https",
-        hostname: "content1.rozetka.com.ua",
-      },
-      {
-        protocol: "https",
-        hostname: "content2.rozetka.com.ua",
-      },
-    ],
     domains: ["img.freepik.com"],
   },
 };
