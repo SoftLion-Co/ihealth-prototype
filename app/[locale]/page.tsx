779998038e0eb942544479f9efbd645d4a501513
Paul <115890528+PaulGraur@users.noreply.github.com>
2024-03-15 11:34:30 +0200

    feature: create folow us section

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import HeroSection from "@/sections/HeroSection";
 import PopularCategoriesSection from "@/app/[locale]/section/PopularCategoriesSection";
 import ServiceSection from "@/app/[locale]/section/ServiceSection";
+import FollowUsSection from "@/app/[locale]/section/FollowUsSection";
 import SubscribeSection from "./section/SubscribeSection";
 
 import { useTranslations } from "next-intl";
@@ -15,6 +16,7 @@ const page = () => {
       <HeroSection />
       <PopularCategoriesSection />
       <ServiceSection />
+      <FollowUsSection />
       <SubscribeSection />
     </main>
   );
---
+++ app/[locale]/section/FollowUsSection.tsx
@@ -0,0 +1,112 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import React, { useRef } from "react";
+
+import image1 from "@/images/test/follow-us/image1.png";
+import image2 from "@/images/test/follow-us/image2.png";
+import image3 from "@/images/test/follow-us/image3.png";
+
+import Title from "@/components/MainTitleComponent";
+import Button from "@/components/ButtonComponent";
+
+import Autoplay from "embla-carousel-autoplay";
+import { useMediaQuery } from "@mui/material";
+import { Carousel } from "@mantine/carousel";
+
+const FollowUsData = [
+  {
+    text: "Follow us on Instagram",
+    href: "https://www.instagram.com/vit_wellness_/",
+    hrefText: "@createx_store",
+    images: [
+      {
+        image: image1,
+        link: "https://www.instagram.com/p/C0JUeAMImxH/",
+      },
+      {
+        image: image2,
+        link: "https://www.instagram.com/p/Cv8LYYqI94V/",
+      },
+      {
+        image: image3,
+        link: "https://www.instagram.com/p/CuoXKgMI2to/",
+      },
+    ],
+  },
+];
+
+const FollowUsSection = () => {
+  const isScreenSmall = useMediaQuery("(max-width: 1440px)");
+  const autoplay = useRef(Autoplay({ delay: 5000 }));
+
+  return (
+    <section className="container">
+      {FollowUsData.map((item, index) => (
+        <div
+          key={index}
+          className="flex flex-col items-center gap-[40px] xl:flex-row xl:justify-between xl:items-start"
+        >
+          <div className="flex flex-col items-center xl:items-start">
+            <p className="text-[16px] mb-[8px] uppercase">{item.text}</p>
+
+            <Link href={item.href} target="_blank">
+              <Title text={item.hrefText} />
+            </Link>
+
+            <Button
+              className="w-max"
+              text="Follow instagram"
+              href={item.href}
+              target="_blank"
+              typeButton="FollowButton"
+              tag="a"
+            />
+          </div>
+
+          {isScreenSmall ? (
+            <Carousel
+              withControls={false}
+              withIndicators
+              loop
+              plugins={[autoplay.current]}
+              onMouseEnter={autoplay.current.stop}
+              onMouseLeave={autoplay.current.reset}
+              className="flex gap-[14px]"
+              classNames={{
+                indicator: "bg-[#17696A] before:hidden",
+                indicators: "bottom-[-22px]",
+              }}
+            >
+              {item.images.map((imageObj, imgIndex) => (
+                <Carousel.Slide className="flex justify-center">
+                  <Link href={imageObj.link} key={imgIndex} target="_blank">
+                    <Image src={imageObj.image} alt={`Image ${imgIndex}`} />
+                  </Link>
+                </Carousel.Slide>
+              ))}
+            </Carousel>
+          ) : (
+            <div className="flex gap-[15px]">
+              {item.images.map((imageObj, imgIndex) => (
+                <div className="flex justify-center">
+                  <Link href={imageObj.link} key={imgIndex} target="_blank">
+                    <Image
+                      src={imageObj.image}
+                      alt={`Image ${imgIndex}`}
+                      width={260}
+                      height={260}
+                      className="w-[260px] h-[260] object-cover"
+                    />
+                  </Link>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      ))}
+    </section>
+  );
+};
+
+export default FollowUsSection;
